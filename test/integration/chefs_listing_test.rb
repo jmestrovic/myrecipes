de5a6f620b6990657302516d40bbf217911ddedc
jmestrovic <jakov.mestrovic@gmail.com>
2017-07-01 16:19:09 +0000

    Chefs listing with test added

---
+++ test/integration/chefs_listing_test.rb
@@ -0,0 +1,16 @@
+require 'test_helper'
+
+class ChefsListingTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @chef = Chef.create!(chefname: "jakov", email: "jakov@invita.hr", password: "password", password_confirmation: "password")
+    @chef2 = Chef.create!(chefname: "elena", email: "elena@invita.hr", password: "password", password_confirmation: "password")
+  end
+  
+  test "should get chefs listing" do
+    get chefs_path
+    assert_template 'chefs/index'
+    assert_select "a[href=?]", chef_path(@chef), text: @chef.chefname.capitalize
+    assert_select "a[href=?]", chef_path(@chef2), text: @chef2.chefname.capitalize
+  end
+end

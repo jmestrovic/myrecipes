843c8f2accec325d6ebdec770c7914a6217cb415
jmestrovic <jakov.mestrovic@gmail.com>
2017-07-05 19:27:10 +0000

    Complete comments feature

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,13 @@ Rails.application.routes.draw do
   root "pages#home"
   get "pages/home", to: "pages#home"
   
-  resources :recipes
   #get '/recipes', to: 'recipes#index'
   #get 'recipes/new', to: 'recipes#new', as: 'new_recipe'
   #get '/recipes/:id', to: 'recipes#show', as: 'recipe'
   #...
+  resources :recipes do
+    resources :comments, only: [:create]
+  end
   
   get "/signup", to: 'chefs#new'
   resources :chefs, except: [ :new ]
